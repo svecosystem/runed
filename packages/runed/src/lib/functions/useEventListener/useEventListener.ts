52126e868a381821309779af3f76ac7cbd3f251c
hugos68 <hugokorteapple@gmail.com>
2024-04-13 19:37:23 +0200

    reverted nullable

--- packages/runed/src/lib/functions/useEventListener/useEventListener.ts
+++ packages/runed/src/lib/functions/useEventListener/useEventListener.ts
@@ -3,14 +3,14 @@ import { boxed } from "$lib/internal/utils/boxed.svelte.js";
 import { addEventListener } from "$lib/internal/utils/event.js";
 
 export function useEventListener<TEvent extends keyof WindowEventMap>(
-	target: ValueOrGetter<Window | null>,
+	target: ValueOrGetter<Window>,
 	event: TEvent,
 	handler: (this: Window, event: WindowEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
 export function useEventListener<TEvent extends keyof DocumentEventMap>(
-	target: ValueOrGetter<Document | null>,
+	target: ValueOrGetter<Document>,
 	event: TEvent,
 	handler: (this: Document, event: DocumentEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
@@ -20,29 +20,26 @@ export function useEventListener<
 	TElement extends HTMLElement,
 	TEvent extends keyof HTMLElementEventMap,
 >(
-	target: ValueOrGetter<TElement | null>,
+	target: ValueOrGetter<TElement>,
 	event: TEvent,
 	handler: (this: TElement, event: HTMLElementEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
 export function useEventListener(
-	target: ValueOrGetter<EventTarget | null>,
+	target: ValueOrGetter<EventTarget>,
 	event: string,
 	handler: EventListenerOrEventListenerObject,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
 export function useEventListener(
-	target: ValueOrGetter<EventTarget | null>,
+	target: ValueOrGetter<EventTarget>,
 	event: string,
 	handler: EventListenerOrEventListenerObject,
 	options?: boolean | AddEventListenerOptions
 ) {
 	const $target = boxed(target);
 
-	$effect(() => {
-		if (!$target.value) return;
-		return addEventListener($target.value, event, handler, options);
-	});
+	$effect(() => addEventListener($target.value, event, handler, options));
 }
