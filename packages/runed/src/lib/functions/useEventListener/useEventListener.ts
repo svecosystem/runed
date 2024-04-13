feefb5b583e7e6dad92ca908a748843cb3718373
hugos68 <hugokorteapple@gmail.com>
2024-04-13 19:36:31 +0200

    Added nullable target

--- packages/runed/src/lib/functions/useEventListener/useEventListener.ts
+++ packages/runed/src/lib/functions/useEventListener/useEventListener.ts
@@ -3,14 +3,14 @@ import { boxed } from "$lib/internal/utils/boxed.svelte.js";
 import { addEventListener } from "$lib/internal/utils/event.js";
 
 export function useEventListener<TEvent extends keyof WindowEventMap>(
-	target: ValueOrGetter<Window>,
+	target: ValueOrGetter<Window | null>,
 	event: TEvent,
 	handler: (this: Window, event: WindowEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
 export function useEventListener<TEvent extends keyof DocumentEventMap>(
-	target: ValueOrGetter<Document>,
+	target: ValueOrGetter<Document | null>,
 	event: TEvent,
 	handler: (this: Document, event: DocumentEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
@@ -20,26 +20,29 @@ export function useEventListener<
 	TElement extends HTMLElement,
 	TEvent extends keyof HTMLElementEventMap,
 >(
-	target: ValueOrGetter<TElement>,
+	target: ValueOrGetter<TElement | null>,
 	event: TEvent,
 	handler: (this: TElement, event: HTMLElementEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
 export function useEventListener(
-	target: ValueOrGetter<EventTarget>,
+	target: ValueOrGetter<EventTarget | null>,
 	event: string,
 	handler: EventListenerOrEventListenerObject,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
 export function useEventListener(
-	target: ValueOrGetter<EventTarget>,
+	target: ValueOrGetter<EventTarget | null>,
 	event: string,
 	handler: EventListenerOrEventListenerObject,
 	options?: boolean | AddEventListenerOptions
 ) {
 	const $target = boxed(target);
 
-	$effect(() => addEventListener($target.value, event, handler, options));
+	$effect(() => {
+		if (!$target.value) return;
+		return addEventListener($target.value, event, handler, options);
+	});
 }
