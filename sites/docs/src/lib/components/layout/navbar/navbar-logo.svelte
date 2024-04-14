5e443863ec5daddf7e4d7885747721487bde4534
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-13 21:30:00 -0400

    update logo, favicon, and og

--- sites/docs/src/lib/components/layout/navbar/navbar-logo.svelte
+++ sites/docs/src/lib/components/layout/navbar/navbar-logo.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+	import RunedLight from "$lib/components/logos/runed-light.svelte";
+	import RunedDark from "$lib/components/logos/runed-dark.svelte";
+</script>
+
 <div class="flex items-center gap-1.5 lg:flex-1">
 	<a
 		href="/"
 		class="flex flex-shrink-0 items-end gap-1.5 text-xl font-bold text-gray-900 dark:text-white"
 	>
-		<p>Runed</p>
-		<span class="sr-only"> Current Page Here </span>
+		<RunedLight class="block h-7 w-auto dark:hidden" />
+		<RunedDark class="hidden h-7 w-auto dark:block" />
+		<span class="sr-only"> Home </span>
 	</a>
 </div>
