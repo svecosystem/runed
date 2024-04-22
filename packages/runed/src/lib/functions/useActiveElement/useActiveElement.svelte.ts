eb8b48d07675857805d2206bdf32bf9d51c855e2
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-04-22 13:39:53 +0100

    fix

--- packages/runed/src/lib/functions/useActiveElement/useActiveElement.svelte.ts
+++ packages/runed/src/lib/functions/useActiveElement/useActiveElement.svelte.ts
@@ -4,7 +4,7 @@ import { isBrowser } from "$lib/internal/utils/browser.js";
  * Returns a reactive value that is equal to `document.activeElement`.
  * It automatically listens for changes, keeping the reference up to date.
  *
- * @returns an object with a reactive value `value` that is equal to `document.activeElement`, or `null` 
+ * @returns an object with a reactive value `value` that is equal to `document.activeElement`, or `null`
  * if there's no active element.
  */
 export function useActiveElement(): { value: Readonly<Element | null> } {
