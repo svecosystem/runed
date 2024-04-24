082c2284df72f9e706d5dcc135c904d1a36ece4b
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-04-24 17:46:37 +0100

    fix exports

--- packages/runed/src/lib/functions/index.ts
+++ packages/runed/src/lib/functions/index.ts
@@ -3,4 +3,4 @@ export * from "./useDebounce/index.js";
 export * from "./useElementSize/index.js";
 export * from "./useEventListener/index.js";
 export * from "./box/index.js";
-export * from "./readonlyBox/index.js";
+
