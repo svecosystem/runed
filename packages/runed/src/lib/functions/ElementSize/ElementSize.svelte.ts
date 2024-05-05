6e5340910e9134495e2a90864fc6b681c70b4604
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-05-05 21:45:36 +0100

    remove leftover

--- packages/runed/src/lib/functions/ElementSize/ElementSize.svelte.ts
+++ packages/runed/src/lib/functions/ElementSize/ElementSize.svelte.ts
@@ -2,7 +2,7 @@ import { box } from "../box/box.svelte.js";
 import type { MaybeBoxOrGetter, MaybeGetter } from "$lib/internal/types.js";
 import { get } from "$lib/internal/utils/get.js";
 
-export type UseElementSizeOptions = {
+export type ElementSizeOptions = {
 	initialSize?: {
 		width: number;
 		height: number;
@@ -21,57 +21,13 @@ export type UseElementSizeOptions = {
  *
  * @see {@link https://runed.dev/docs/functions/use-element-size}
  */
-export function useElementSize(
-	_node: MaybeBoxOrGetter<HTMLElement | undefined>,
-	options: UseElementSizeOptions = {
-		box: "border-box",
-	}
-): { width: number; height: number } {
-	const node = box.from(_node);
-	const size = $state({
-		width: options.initialSize?.width ?? 0,
-		height: options.initialSize?.height ?? 0,
-	});
-
-	$effect(() => {
-		if (!node.value) return;
-
-		const observer = new ResizeObserver((entries) => {
-			for (const entry of entries) {
-				const boxSize = options.box === "content-box" ? entry.contentBoxSize : entry.borderBoxSize;
-				const boxSizeArr = Array.isArray(boxSize) ? boxSize : [boxSize];
-				size.width = boxSizeArr.reduce((acc, size) => Math.max(acc, size.inlineSize), 0);
-				size.height = boxSizeArr.reduce((acc, size) => Math.max(acc, size.blockSize), 0);
-			}
-		});
-		observer.observe(node.value);
-
-		return () => {
-			observer.disconnect();
-		};
-	});
-
-	return size;
-}
-
-/**
- * Returns a reactive value holding the size of `node`.
- *
- * Accepts an `options` object with the following properties:
- * - `initialSize`: The initial size of the element. Defaults to `{ width: 0, height: 0 }`.
- * - `box`: The box model to use. Can be either `"content-box"` or `"border-box"`. Defaults to `"border-box"`.
- *
- * @returns an object with `width` and `height` properties.
- *
- * @see {@link https://runed.dev/docs/functions/use-element-size}
- */
 export class ElementSize {
 	#size = $state({
 		width: 0,
 		height: 0,
 	})
 
-	constructor(node: MaybeGetter<HTMLElement | undefined>, options: UseElementSizeOptions = { box: "border-box" }) {
+	constructor(node: MaybeGetter<HTMLElement | undefined>, options: ElementSizeOptions = { box: "border-box" }) {
 		this.#size = {
 			width: options.initialSize?.width ?? 0,
 			height: options.initialSize?.height ?? 0,
