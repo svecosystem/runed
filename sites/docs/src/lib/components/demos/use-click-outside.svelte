f77e853e4c62670c2817b097644590bcc25d3ef0
Bohdan Sviripa <sviripa.work@gmail.com>
2024-05-07 20:48:47 -0400

    add docs page

---
+++ sites/docs/src/lib/components/demos/use-click-outside.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { box, useClickOutside } from "runed";
+
+	let containerText = "Has not clicked yet";
+
+	const container = box<HTMLDivElement | null>(null);
+
+	useClickOutside(container, () => {
+		containerText = "Has clicked outside of container";
+	});
+</script>
+
+<main>
+	<div bind:this={container.value}>
+		<p>{containerText}</p>
+
+		<button onclick={() => (containerText = "Has clicked within container")}>
+			Button within container
+		</button>
+	</div>
+	<button>Button outside container</button>
+</main>
+
+<style>
+	div {
+		padding: 40px;
+		border: 1px solid;
+		margin-bottom: 16px;
+	}
+
+	button {
+		border: 1px solid;
+		padding-inline: 8px;
+	}
+</style>
