69780018c063959a6ad5842a86e258625d7b9e1f
tglide <26071571+TGlide@users.noreply.github.com>
2024-05-11 23:04:00 +0100

    update svelte and fix hacky hack

--- sites/docs/src/lib/components/demos/active-element.svelte
+++ sites/docs/src/lib/components/demos/active-element.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import { activeElement } from "runed";
-	import { browser } from "$app/environment";
 </script>
 
 <div class="rounded-md bg-card p-8">
 	<p>
 		Currently active element:
 		<span class="font-bold">
-			{#if browser}
-				{activeElement.current?.localName ?? "No active element found"}
-			{/if}
+			{activeElement.current?.localName ?? "No active element found"}
 		</span>
 	</p>
 </div>
