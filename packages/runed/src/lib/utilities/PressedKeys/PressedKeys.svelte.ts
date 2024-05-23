9a5d03d2fa29c2b8b86dd001ce50269683770572
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-05-23 23:09:14 +0100

    Pressed keys (#58)
    * poc
    
    * pressed keys demo
    
    * demo adjustments
    
    * jsdoc
    
    * docs
    
    * changeset
    
    * lint
    
    * add even more flare to demo
    
    * rename pressed to has
    
    * format

---
+++ packages/runed/src/lib/utilities/PressedKeys/PressedKeys.svelte.ts
@@ -0,0 +1,43 @@
+import { addEventListener } from "$lib/internal/utils/event.js";
+
+/**
+ * Tracks which keys are currently pressed.
+ *
+ * @see {@link https://runed.dev/docs/utilities/pressed-keys}
+ */
+export class PressedKeys {
+	#pressedKeys = $state<Array<string>>([]);
+
+	constructor() {
+		$effect(() => {
+			const callbacks: VoidFunction[] = [];
+
+			callbacks.push(
+				addEventListener(window, "keydown", (e) => {
+					const key = e.key.toLowerCase();
+					if (!this.#pressedKeys.includes(key)) {
+						this.#pressedKeys.push(key);
+					}
+				})
+			);
+
+			callbacks.push(
+				addEventListener(window, "keyup", (e) => {
+					const key = e.key.toLowerCase();
+					this.#pressedKeys = this.#pressedKeys.filter((k) => k !== key);
+				})
+			);
+
+			return () => callbacks.forEach((c) => c());
+		});
+	}
+
+	has(...keys: string[]) {
+		const normalizedKeys = keys.map((key) => key.toLowerCase());
+		return normalizedKeys.every((key) => this.#pressedKeys.includes(key));
+	}
+
+	get all() {
+		return this.#pressedKeys;
+	}
+}
