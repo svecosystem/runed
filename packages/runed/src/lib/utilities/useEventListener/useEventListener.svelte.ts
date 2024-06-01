9b0769906de45145c4f43e53fd00c3ff0b940c35
Abdelrahman <abdelrahmanb015@gmail.com>
2024-06-01 15:44:02 +0300

    Improve Media Query Utility (#64)
    * feat: add MediaQuery utility
    
    * chore: add changesets
    
    * refactor
    
    * make MediaQuery readonly
    
    ---------
    
    Co-authored-by: paoloricciuti <ricciutipaolo@gmail.com>
    Co-authored-by: Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>

--- packages/runed/src/lib/utilities/useEventListener/useEventListener.svelte.ts
+++ packages/runed/src/lib/utilities/useEventListener/useEventListener.svelte.ts
@@ -18,6 +18,15 @@ export function useEventListener<TEvent extends keyof WindowEventMap>(
 	options?: boolean | AddEventListenerOptions
 ): void;
 
+/**
+ * Adds an event listener to the specified target element for the given event(s), and returns a function to remove it.
+ * @param target The target element to add the event listener to.
+ * @param event The event(s) to listen for.
+ * @param handler The function to be called when the event is triggered.
+ * @param options An optional object that specifies characteristics about the event listener.
+ *
+ * @see {@link https://runed.dev/docs/utilities/use-event-listener}
+ */
 export function useEventListener<TEvent extends keyof DocumentEventMap>(
 	target: MaybeGetter<Document | null | undefined>,
 	event: TEvent | TEvent[],
@@ -41,6 +50,22 @@ export function useEventListener<
 	target: MaybeGetter<TElement | null | undefined>,
 	event: TEvent | TEvent[],
 	handler: (this: TElement, event: HTMLElementEventMap[TEvent]) => unknown,
+	options?: boolean | AddEventListenerOptions
+): void;
+
+/**
+ * Adds an event listener to the specified target element for the given event(s), and returns a function to remove it.
+ * @param target The target element to add the event listener to.
+ * @param event The event(s) to listen for.
+ * @param handler The function to be called when the event is triggered.
+ * @param options An optional object that specifies characteristics about the event listener.
+ *
+ * @see {@link https://runed.dev/docs/utilities/use-event-listener}
+ */
+export function useEventListener<TEvent extends keyof MediaQueryListEventMap>(
+	target: MaybeGetter<MediaQueryList | null | undefined>,
+	event: TEvent | TEvent[],
+	handler: (this: MediaQueryList, event: MediaQueryListEventMap[TEvent]) => unknown,
 	options?: boolean | AddEventListenerOptions
 ): void;
 
