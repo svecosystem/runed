9ba28ae62b493ce092a7dabef35ce3c033dbb2a4
Abdelrahman <abdelrahmanb015@gmail.com>
2024-06-01 15:44:02 +0300

    Improve Media Query Utility (#64)
    * feat: add MediaQuery utility
    
    * chore: add changesets
    
    * refactor
    
    * make MediaQuery readonly
    
    ---------
    
    Co-authored-by: paoloricciuti <ricciutipaolo@gmail.com>
    Co-authored-by: Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>

--- packages/runed/src/lib/utilities/MediaQuery/MediaQuery.svelte.ts
+++ packages/runed/src/lib/utilities/MediaQuery/MediaQuery.svelte.ts
@@ -1,3 +1,7 @@
+import { extract } from "../extract/extract.js";
+import { useEventListener } from "../useEventListener/useEventListener.svelte.js";
+import type { MaybeGetter } from "$lib/internal/types.js";
+
 /**
  * Take a media query (or a function that returns one if you want reactivity)
  * as input and you can check if it currently matches doing `instance.match`
@@ -8,45 +12,43 @@
  * ```ts
  * const screen = new MediaQuery("(min-width: 640px)");
  *
- * $effect(()=>{
- * 	if(screen.match){
+ * $effect(() => {
+ * 	if (screen.match) {
  * 		console.log("The screen is less than 640px");
  * 	}
  * });
  * ```
- * 
+ *
  * @example
  * ```ts
  * let media = $state("(min-width: 640px)");
  * const screen = new MediaQuery(()=> media);
  *
- * $effect(()=>{
- * 	if(screen.match){
+ * $effect(() => {
+ * 	if (screen.match) {
  * 		console.log(`Media query ${media} is ${screen.match}`);
  * 	}
  * });
- * 
+ *
  * media = "(min-width: 320px)";
  * ```
-
  */
 export class MediaQuery {
-	match = $state<boolean | undefined>(undefined);
+	#match: boolean | undefined = $state();
 
-	constructor(query: string | (() => string)) {
+	constructor(query: MaybeGetter<string>) {
 		$effect(() => {
-			const result = window.matchMedia(typeof query === "function" ? query() : query);
+			const result = window.matchMedia(extract(query));
 
-			this.match = result.matches;
+			this.#match = result.matches;
 
-			const listener = (changed: MediaQueryListEvent) => {
-				this.match = changed.matches;
-			};
-
-			result.addEventListener("change", listener);
-			return () => {
-				result.removeEventListener("change", listener);
-			};
+			useEventListener(result, "change", (changed) => {
+				this.#match = changed.matches;
+			});
 		});
+	}
+
+	get match(): boolean | undefined {
+		return this.#match;
 	}
 }
