3d8bc569be15741106dfe492dc5cefe8210ea3c0
Tyson <TysonS.417@gmail.com>
2024-06-06 19:45:45 -0700

    remove default values

--- packages/runed/src/lib/utilities/IsIdle/IsIdle.svelte.ts
+++ packages/runed/src/lib/utilities/IsIdle/IsIdle.svelte.ts
@@ -4,68 +4,52 @@ const DEFAULT_EVENTS = [
 	"touchmove",
 	"click",
 	"scroll",
-] satisfies (keyof DocumentEventMap)[];
-
+] satisfies (keyof WindowEventMap)[];
 
 const DEFAULT_OPTIONS = {
 	events: DEFAULT_EVENTS,
 	initialState: false,
 };
 
-// TODO: give this a real description
 /**
- * Function that takes a callback, and returns a debounced version of it.
- * When calling the debounced function, it will wait for the specified time
- * before calling the original callback. If the debounced function is called
- * again before the time has passed, the timer will be reset.
- *
- * You can await the debounced function to get the value when it is eventually
- * called.
- *
- * The second parameter is the time to wait before calling the original callback.
- * Alternatively, it can also be a getter function that returns the time to wait.
- *
+ * Tracks whether the user is being inactive.
+ * @see {@link https://runed.dev/docs/utilities/is-idle}
  */
 export class IsIdle {
-	#idle = $state(false);
+	#idle = $state<boolean>();
 	#timer = $state<number>();
-	#events;
+	#timeout = $state<number>();
+	#events: (keyof WindowEventMap)[];
 
-	constructor(
-		timeout: number = 500,
-		options: Partial<typeof DEFAULT_OPTIONS> = DEFAULT_OPTIONS
-	) {
+	constructor(timeout: number = 500, options: Partial<typeof DEFAULT_OPTIONS> = DEFAULT_OPTIONS) {
+		this.#events = options?.events || ["keypress", "mousemove", "touchmove", "click", "scroll"];
+		this.#idle = options?.initialState ?? false;
+		this.#timeout = timeout;
 
-    this.#events = options?.events || ["keypress", "mousemove", "touchmove", "click", "scroll"];
-    this.#idle = options?.initialState || false;
+		$effect(() => {
+			this.#events.forEach((event) => document.addEventListener(event, this.reset));
 
-    $effect(() => {
-      const handleEvents = () => {
-        this.#idle = false
-  
-        if (this.#timer) {
-          window.clearTimeout(this.#timer);
-        }
-  
-        this.#timer = window.setTimeout(() => {
-          this.#idle = true
-        }, timeout);
-      };
-  
-      this.#events.forEach((event) => document.addEventListener(event, handleEvents));
-  
-      // remove event listeners onUnmount
-      return () => {
-        this.#events.forEach((event) => document.removeEventListener(event, handleEvents));
-      };
-    });
-  }
+			// remove event listeners onUnmount
+			return () => {
+				this.#events.forEach((event) => document.removeEventListener(event, this.reset));
+			};
+		});
+	}
 
+  // allow consumer to manually reset state
+	reset() {
+		this.#idle = false;
 
-  get current() {
+		if (this.#timer) {
+			window.clearTimeout(this.#timer);
+		}
+
+		this.#timer = window.setTimeout(() => {
+			this.#idle = true;
+		}, this.#timeout);
+	}
+
+	get current() {
 		return this.#idle;
 	}
 }
-
-
-
