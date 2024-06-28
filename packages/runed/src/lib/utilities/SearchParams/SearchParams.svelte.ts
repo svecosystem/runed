407a2634d94d1149015e74fc82824feae3085aa8
tglide <26071571+TGlide@users.noreply.github.com>
2024-06-28 10:16:03 +0100

    checkpoint

--- packages/runed/src/lib/utilities/SearchParams/SearchParams.svelte.ts
+++ packages/runed/src/lib/utilities/SearchParams/SearchParams.svelte.ts
@@ -1,12 +1,21 @@
 import { Store } from "../Store/Store.svelte.js";
-import { useEventListener } from "../useEventListener/useEventListener.svelte.js";
 import { watch } from "../watch/watch.svelte.js";
 import { page } from "$app/stores";
 import { entries } from "$lib/internal/utils/object.js";
 import { goto } from "$app/navigation";
 
+export type SearchParamsSingleOptions<T> = {
+	encode?: (value: T) => string;
+	decode?: (value: string | null) => T;
+	defaultValue?: T;
+};
+
+export type SearchParamsAllOptions<T> = {
+	[key: string]: SearchParamsSingleOptions<T>;
+};
+
 export class SearchParams {
-	#params: Record<string, string> = $state({});
+	#params: Record<string, string | null> = $state({});
 	#page = new Store(page);
 
 	// Prohibit using this constructor
@@ -35,11 +44,30 @@ export class SearchParams {
 	}
 
 	get params() {
+		// TODO: use dynamic setters and getters instead to increase efficiency?
 		return this.#params;
 	}
 
 	static all() {
 		const instance = new SearchParams();
 		return instance.params;
+	}
+
+	static single<T>(key: string, options: SearchParamsSingleOptions<T> = {}) {
+		// TODO: make encode and decode type safe
+		const instance = new SearchParams();
+		const decode = options.decode ?? ((v) => v);
+		// eslint-disable-next-line ts/no-explicit-any
+		const encode: (v: any) => string = options.encode ?? ((v: unknown) => `${v}`);
+		instance.params[key] = encode(options.defaultValue ?? null)
+
+		return {
+			get current() {
+				return decode(instance.params[key] ?? null) as T
+			},
+			set current(value: T) {
+				instance.params[key] = options.encode?.(value) ?? `${value}`;
+			},
+		};
 	}
 }
--- sites/docs/src/lib/components/demos/search-params.svelte
+++ sites/docs/src/lib/components/demos/search-params.svelte
@@ -14,16 +14,19 @@
 	}
 
 	function setParams() {
-		console.log(params)
 		params.a = 2;
 		params.b = Math.random();
 		params.c = Math.random();
 	}
+
+	const pageParams = $derived(page.current.url.href.split("?")[1]);
+	const count = SearchParams.single("count", { defaultValue: 0, decode: (v) => Number(v ?? 0) });
 </script>
 
 <DemoContainer>
-	<pre>{page.current.url.href}</pre>
+	<pre>{JSON.stringify(pageParams, null, 2)}</pre>
 	<pre>{JSON.stringify(params, null, 2)}</pre>
 	<Button onclick={setPage}>set with page store</Button>
 	<Button onclick={setParams}>set with SearchParams</Button>
+	<Button onclick={() => count.current++}>increment count {count.current} {typeof count.current}</Button>
 </DemoContainer>
