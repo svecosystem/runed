8bc231c2fbc0643ecc3baf9b22c4970385f6ec9a
Idan Gazit <idan@gazit.me>
2024-07-02 23:29:00 +0300

    chore: address review feedback! ✨
    Thanks @TGlide for the excellent review.

--- packages/runed/src/lib/utilities/FiniteStateMachine/FiniteStateMachine.svelte.ts
+++ packages/runed/src/lib/utilities/FiniteStateMachine/FiniteStateMachine.svelte.ts
@@ -1,3 +1,60 @@
+export type FSMLifecycleFn<StatesT extends string, EventsT extends string> = (
+	meta: LifecycleFnMeta<StatesT, EventsT>
+) => void;
+
+export type LifecycleFnMeta<StatesT extends string, EventsT extends string> = {
+	from: StatesT | null;
+	to: StatesT;
+	event: EventsT | null;
+	args: unknown;
+};
+
+export function isLifecycleFnMeta<StatesT extends string, EventsT extends string>(
+	meta: unknown
+): meta is LifecycleFnMeta<StatesT, EventsT> {
+	return (
+		!!meta &&
+		typeof meta === "object" &&
+		"to" in meta &&
+		"from" in meta &&
+		"event" in meta &&
+		"args" in meta
+	);
+}
+
+export type FSMLifecycle = "_enter" | "_exit";
+
+// Actions are either a StateT (string)
+// or a function that optionally returns a StateT
+export type ActionFn<StatesT> = (...args: unknown[]) => StatesT | void;
+export type Action<StatesT> = StatesT | ActionFn<StatesT>;
+
+// State handlers are objects that map events to actions
+// or lifecycle functions to handlers
+export type StateHandler<StatesT extends string, EventsT extends string> = {
+	[e in EventsT]?: Action<StatesT>;
+} & {
+	[k in FSMLifecycle]?: FSMLifecycleFn<StatesT, EventsT>;
+};
+
+export type Transition<StatesT extends string, EventsT extends string> = {
+	[s in StatesT]: StateHandler<StatesT, EventsT>;
+} & {
+	// '*' is a special fallback handler state.
+	// If no handler is found on the current state and the '*' state exists,
+	// the handler from the '*' state will be used.
+	// We can't put the '*' in the same object, has to be an intersection or
+	// the typescript compiler will complain about mapped types not being
+	// able to declare properties or methods
+	"*"?: StateHandler<StatesT, EventsT>;
+};
+
+/**
+ * Finite State Machine
+ * @param initial - The initial state of the FSM
+ * @param states - The transition object that maps states to events and actions
+ * @returns A FiniteStateMachine object
+ */
 export class FiniteStateMachine<StatesT extends string, EventsT extends string> {
 	#current = $state() as StatesT;
 	readonly states: Transition<StatesT, EventsT>;
@@ -27,14 +84,12 @@ export class FiniteStateMachine<StatesT extends string, EventsT extends string> 
 					console.warn("Invalid metadata passed to lifecycle function of the FSM.");
 				}
 			} else {
-				return (action as (...args: unknown[]) => void | StatesT)(...args);
+				return (action as ActionFn<StatesT>)(...args);
 			}
 		} else if (typeof action === "string") {
 			return action as StatesT;
-		} else {
-			if (event !== "_enter" && event !== "_exit") {
-				console.warn("No action defined for event", event, "in state", this.#current);
-			}
+		} else if (event !== "_enter" && event !== "_exit") {
+			console.warn("No action defined for event", event, "in state", this.#current);
 		}
 	}
 
@@ -63,53 +118,3 @@ export class FiniteStateMachine<StatesT extends string, EventsT extends string> 
 		return this.#current;
 	}
 }
-
-export type FSMLifecycleFn<StatesT extends string, EventsT extends string> = (
-	meta: LifecycleFnMeta<StatesT, EventsT>
-) => void;
-
-export type LifecycleFnMeta<StatesT extends string, EventsT extends string> = {
-	from: StatesT | null;
-	to: StatesT;
-	event: EventsT | null;
-	args: unknown;
-};
-
-export function isLifecycleFnMeta<StatesT extends string, EventsT extends string>(
-	meta: unknown
-): meta is LifecycleFnMeta<StatesT, EventsT> {
-	return (
-		!!meta &&
-		typeof meta === "object" &&
-		"to" in meta &&
-		"from" in meta &&
-		"event" in meta &&
-		"args" in meta
-	);
-}
-
-export type FSMLifecycle = "_enter" | "_exit";
-
-// Actions are either a StateT (string)
-// or a function that optionally returns a StateT
-export type Action<StatesT> = StatesT | ((...args: unknown[]) => StatesT | void);
-
-// State handlers are objects that map events to actions
-// or lifecycle functions to handlers
-export type StateHandler<StatesT extends string, EventsT extends string> = {
-	[e in EventsT]?: Action<StatesT>;
-} & {
-	[k in FSMLifecycle]?: FSMLifecycleFn<StatesT, EventsT>;
-};
-
-export type Transition<StatesT extends string, EventsT extends string> = {
-	[s in StatesT]: StateHandler<StatesT, EventsT>;
-} & {
-	// '*' is a special fallback handler state.
-	// If no handler is found on the current state and the '*' state exists,
-	// the handler from the '*' state will be used.
-	// We can't put the '*' in the same object, has to be an intersection or
-	// the typescript compiler will complain about mapped types not being
-	// able to declare properties or methods
-	"*"?: StateHandler<StatesT, EventsT>;
-};
