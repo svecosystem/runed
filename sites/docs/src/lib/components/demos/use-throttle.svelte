cfe7384974116f77737d7406b032578e2eae24b7
Jayden Carey <jaydenaus9@gmail.com>
2024-07-07 03:35:41 +0800

    add throttled

--- sites/docs/src/lib/components/demos/use-throttle.svelte
+++ sites/docs/src/lib/components/demos/use-throttle.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
 	import { useThrottle } from "runed";
 
+	import { Label } from "../ui/label";
 	import DemoContainer from "$lib/components/demo-container.svelte";
 	import Input from "$lib/components/ui/input/input.svelte";
 
 	let search = $state("");
 	let throttledSearch = $state("");
+	let durationMs = $state(1000);
 
-	const something = useThrottle(() => {
-		throttledSearch = search;
-	}, 2000);
+	const something = useThrottle(
+		() => {
+			throttledSearch = search;
+		},
+		() => durationMs
+	);
 
 	$effect(() => {
 		// eslint-disable-next-line no-unused-expressions
 		search;
 		something();
 	});
-
-	$effect(() => {
-		console.log(`Pending is ${something.pending}`);
-	});
 </script>
 
 <DemoContainer class="flex flex-col gap-4">
-	<Input bind:value={search} placeholder="Search the best utilities for Svelte 5" />
-	<p>Search value: {search} {something.pending}</p>
-	<p>Throttled search value: {throttledSearch}</p>
+	<div class="flex flex-col gap-1.5">
+		<Label for="duration">Throttle duration (ms)</Label>
+		<Input id="duration" type="number" bind:value={durationMs} />
+	</div>
+
+	<div class="flex flex-col gap-1.5">
+		<Label for="search">Search</Label>
+		<Input bind:value={search} placeholder="Search the best utilities for Svelte 5" />
+	</div>
+	<p>Search value: {search}</p>
+
 	<p>
 		{#if throttledSearch}
 			You searched for: <b>{throttledSearch}</b>
