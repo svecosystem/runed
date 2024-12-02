ee22ea0954de1f7b2c545815b9aa4123e17dbed1
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-12-02 11:21:16 -0500

    docs: plausible analytics (#155)

--- sites/docs/src/routes/+layout.svelte
+++ sites/docs/src/routes/+layout.svelte
@@ -2,10 +2,17 @@
 	import "../app.css";
 	import { siteConfig } from "$lib/config/site";
 	import { useSiteConfig } from "@svecodocs/kit";
+	import { dev } from "$app/environment";
 
 	let { children } = $props();
 
 	useSiteConfig(() => siteConfig);
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<script defer data-domain="runed.dev" src="https://server.hj.run/js/script.js"></script>
+	{/if}
+</svelte:head>
 
 {@render children?.()}
