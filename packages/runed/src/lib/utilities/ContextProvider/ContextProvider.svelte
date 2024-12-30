5531dbfcfd8e041de03de17448f5d121c444f97a
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-12-30 13:22:21 +0200

    add ContextProvider

---
+++ packages/runed/src/lib/utilities/ContextProvider/ContextProvider.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" generics="TContext">
+	import type { ContextProviderProps } from "./types.js";
+
+	const { context, value, children }: ContextProviderProps<TContext> = $props();
+
+	context.set(value);
+</script>
+
+{@render children()}
