e7c4fc38806ccd615833c40b438a027226daed53
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-12-30 13:25:24 +0200

    shut up eslint

--- packages/runed/src/lib/utilities/ContextProvider/ContextProvider.svelte
+++ packages/runed/src/lib/utilities/ContextProvider/ContextProvider.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" generics="TContext">
 	import type { ContextProviderProps } from "./types.js";
 
+	// eslint-disable-next-line no-undef
 	const { context, value, children }: ContextProviderProps<TContext> = $props();
 
 	context.set(value);
