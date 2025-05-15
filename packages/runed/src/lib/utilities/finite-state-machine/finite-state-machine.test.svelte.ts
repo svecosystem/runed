1777f6b63dcecef7b1378838f8144c56eeeabc87
James Garbutt <43081j@users.noreply.github.com>
2025-05-15 16:54:23 +0100

    chore: update vite & setup browser tests (#255)
    * chore: update vite & add browser test support
    
    This updates vitest and friends, and enables browser test support (which
    we have none of _yet_).
    
    * deploy cf site with `wrangler@4`
    
    * upload the whole `.svelte-kit` dir
    
    * `.svelte-kit` is considered a hidden file...
    
    ---------
    
    Co-authored-by: AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>

--- packages/runed/src/lib/utilities/finite-state-machine/finite-state-machine.test.svelte.ts
+++ packages/runed/src/lib/utilities/finite-state-machine/finite-state-machine.test.svelte.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { type Action, FiniteStateMachine } from "./finite-state-machine.svelte.js";
 
 describe("FiniteStateMachine", () => {
