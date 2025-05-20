395eeb6cf19410fb33eaceaf0d55e193fe89230a
David Peng <davipondev@gmail.com>
2025-05-20 21:31:58 +0800

    remove unused demo component

--- sites/docs/src/lib/components/demos/use-search-params.svelte
+++
@@ -1,9 +0,0 @@
-
-<script lang="ts">
-	import { useSearchParams } from 'runed';
-	import { DemoContainer } from '@svecodocs/kit';
-</script>
-
-<DemoContainer>
-	<!-- Add your demo here -->
-</DemoContainer>
