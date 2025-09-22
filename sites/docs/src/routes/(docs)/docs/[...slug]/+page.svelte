88fb82e476bcac23826a098c0f336f16fa3f7d1b
Hunter Johnston <johnstonhuntera@gmail.com>
2025-09-21 21:10:25 -0400

    docs: plz

--- sites/docs/src/routes/(docs)/docs/[...slug]/+page.svelte
+++ sites/docs/src/routes/(docs)/docs/[...slug]/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+	import { page } from "$app/state";
 	import { DocPage } from "@svecodocs/kit";
 	let { data } = $props();
+
+	const ogUrl = $derived(
+		`${page.url.origin}/og?title=${encodeURIComponent(data.metadata.title)}&description=${encodeURIComponent(data.metadata.description)}`
+	);
 </script>
 
 <svelte:head>
 	<title>{data.metadata.title}</title>
 	<meta name="description" content={data.metadata.description} />
 </svelte:head>
-<DocPage component={data.component} {...data.metadata} />
+<DocPage
+	component={data.component}
+	{...data.metadata}
+	metadata={{ ogImage: { url: ogUrl, width: "1200", height: "630" } }}
+/>
